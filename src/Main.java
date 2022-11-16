87a21f63f7d00456daa607a080cbbdbae17e65f9
senoritafia <sophiad.1243@gmail.com>
2022-11-16 14:06:34 -0600

    Added more edits!

--- src/Main.java
+++ src/Main.java
@@ -32,14 +32,16 @@ public class Main {
         symptomID.put("Aches", 8);
         symptomID.put("Low Appetite", 9);
         symptomID.put("Rash", 10);
-        symptomID.put("Swollen lymph nodes", 12);
-        symptomID.put("Swollen tonsils", 13);
-        symptomID.put("Cough", 14);
-        symptomID.put("Chills", 15);
-        symptomID.put("Shortness of breath", 16);
+        symptomID.put("Swollen Lymph Nodes", 11);
+        symptomID.put("Swollen Tonsils", 12);
+        symptomID.put("Cough", 13);
+        symptomID.put("Chills", 14);
+        symptomID.put("Shortness Of Breath", 15);
 
-// creating array
-        int[] diseaseIDArray= new int [16];
+        // creating array
+        List<Integer> diseaseIDList = new ArrayList<>();
+        //diseaseIDList.add(2);
+        //[] diseaseIDArray= new int [16];
         // see if map contains symptom name
 
         //Error here, come back, for some reason it is always true
@@ -48,9 +50,100 @@ public class Main {
         if (hasName1 = true) {
             int id = symptomID.get("Loose Stool");
             System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName2 = symptomList.contains("Vomiting");
+        if (hasName2 == true) {
+            int id = symptomID.get("Vomiting");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
 
+        boolean hasName3 = symptomList.contains("Abdominal Cramps");
+        if (hasName3 == true) {
+            int id = symptomID.get("Abdominal Cramps");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName4 = symptomList.contains("Nausea");
+        if (hasName4 == true) {
+            int id = symptomID.get("Nausea");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName5 = symptomList.contains("Fever");
+        if (hasName5 == true) {
+            int id = symptomID.get("Fever");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName6 = symptomList.contains("Fatigue");
+        if (hasName6 == true) {
+            int id = symptomID.get("Fatigue");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName7 = symptomList.contains("Sore Throat");
+        if (hasName7 == true) {
+            int id = symptomID.get("Sore Throat");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName8 = symptomList.contains("Aches");
+        if (hasName8 == true) {
+            int id = symptomID.get("Aches");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName9 = symptomList.contains("Low Appetite");
+        if (hasName9 == true) {
+            int id = symptomID.get("Low Appetite");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName10 = symptomList.contains("Rash");
+        if (hasName10 == true) {
+            int id = symptomID.get("Rash");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName11 = symptomList.contains("Swollen Lymph Nodes");
+        if (hasName11 == true) {
+            int id = symptomID.get("Swollen Lymph Nodes");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName12 = symptomList.contains("Swollen Tonsils");
+        if (hasName12 == true) {
+            int id = symptomID.get("Swollen Tonsils");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName13 = symptomList.contains("Cough");
+        if (hasName13 == true) {
+            int id = symptomID.get("Cough");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName14 = symptomList.contains("Chills");
+        if (hasName14 == true) {
+            int id = symptomID.get("Chills");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
+        boolean hasName15 = symptomList.contains("Shortness Of Breath");
+        if (hasName15 == true) {
+            int id = symptomID.get("Shortness Of Breath");
+            System.out.println(id);
+            diseaseIDList.add(id);
+        }
 
-
+        System.out.println(diseaseIDList);
+/*
+        boolean hasName1 = symptomID.containsKey("Loose Stool");
+        if (hasName1 = true) {
+            System.out.println("hello!");
+            }
         boolean hasName2 = symptomID.containsKey("Vomiting");
         if (hasName2 = true) {
             System.out.println("hello!");
@@ -109,7 +202,7 @@ public class Main {
             System.out.println("hi!");
         }
 
-
+*/
 
     }
 }
