f8da305a3e41751fa454df0197f7518bd2f4877f
senoritafia <sophiad.1243@gmail.com>
2023-01-06 08:45:54 -0600

    :D new code updates

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,10 @@ public class Main {
 
         ArrayList<Disease> diseaseList = new ArrayList<>();
         diseaseList.add(new Disease("Diarrhea", new int[]{1, 2, 3, 4, 5}, new int[]{4, 1, 3, 2, 1}));
-        diseaseList.add(new Disease("Mononucleosis", new int[]{5, 6, 8, 9, 10}, new int[]{4, 4, 3, 2, 1}));
-
-
+        diseaseList.add(new Disease("Mononucleosis", new int[]{5, 6, 7, 8, 9, 10, 11}, new int[]{4, 3, 2, 3, 2, 1, 3}));
+        diseaseList.add(new Disease("Chickenpox", new int[]{5, 6, 8, 9, 10}, new int[]{2, 2, 3, 3, 4}));
+        diseaseList.add(new Disease("Strep Throat", new int[]{2, 5, 7, 11, 12}, new int[]{2, 3, 4, 3, 3}));
+        diseaseList.add(new Disease("Covid-19", new int[]{5, 6, 13, 14, 15}, new int[]{4, 3, 1, 2, 3}));
         //initializes scanner
         Scanner symptomScanner = new Scanner(System.in);
         System.out.println("Enter your symptoms! Separate symptoms with a comma!");
@@ -271,7 +272,7 @@ public class Main {
         for(Disease disease : diseaseList) {
             for (Symptom symptom : patientSymptoms) {
                 if (disease.hasSymptom(symptom.id())) {
-                    System.out.printf("%s has symptom %s with weight %d", disease.name(), symptom.name(), disease.symptomWeight(symptom.id()));
+                    System.out.printf("%s has symptom %s with weight %d ", disease.name(), symptom.name(), disease.symptomWeight(symptom.id()));
                 };
             }
         }
