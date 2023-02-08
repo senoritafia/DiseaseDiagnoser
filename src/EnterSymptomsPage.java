b5c8fa8b6a99a428f98b7237fe4c831b36583ec3
senoritafia <sophiad.1243@gmail.com>
2023-02-07 18:38:31 -0600

    :D new code updates

--- src/EnterSymptomsPage.java
+++ src/EnterSymptomsPage.java
@@ -3,44 +3,48 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DiagnosisPage implements ActionListener {
+public class EnterSymptomsPage implements ActionListener {
     int count = 0;
     JLabel label;
     JFrame frame;
     JButton button;
+    JTextField textField;
 
-    public DiagnosisPage() {
+    public EnterSymptomsPage() {
         frame = new JFrame();
 
-        button = new JButton("Exit");
+        button = new JButton("Click Here To Get Your Diagnosis!");
         button.addActionListener(this);
-
-        label = new JLabel("insert diagnosis here");
+        label = new JLabel("Please enter symptoms below. Remember that the more symptoms you give, the more accurate of a diagnosis the machine can give you. Please seperate symptoms with a comma");
+        textField = new JTextField();
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
         panel.add(label);
-        panel.setBackground(Color.PINK);
+        panel.add(textField);
+        panel.setBackground(Color.RED);
+
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Diagnosis Page");
+        frame.setTitle("Enter Symptoms Page");
         frame.pack();
         frame.setVisible(true);
 
-
+        //textField.setFont(times new roman);
 
     }
 
     public static void main(String[] args) {
-        new DiagnosisPage();
+        new EnterSymptomsPage();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         frame.dispose();
+        DiagnosisPage DiagnosisPage1 = new DiagnosisPage();
 
     }
 }
--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -6,16 +6,18 @@ public class WelcomePage implements ActionListener{
     JLabel label;
     JFrame frame;
     JButton button;
+
     public WelcomePage() {
         frame=new JFrame();
 
         button=new JButton("Start");
         button.addActionListener(this);
+        button.setSize(300,100);
 
-        label=new JLabel("Number of clicks:0");
+        label=new JLabel("Welcome To The Disease Diagnoser!");
 
         JPanel panel =new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
         panel.setLayout(new GridLayout(0,1));
         panel.add(button);
         panel.add(label);
@@ -23,9 +25,11 @@ public class WelcomePage implements ActionListener{
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame.setTitle("Disease Diagnoser");
+        frame.setTitle("Welcome Page");
         frame.pack();
         frame.setVisible(true);
+
+
     }
     public static void main(String[]args){
         new WelcomePage();
@@ -33,7 +37,7 @@ public class WelcomePage implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        dispose();
-        DiagnosisPage secondFrame = new DiagnosisPage();
+        frame.dispose();
+        EnterSymptomsPage EnterSymptomsPage1 = new EnterSymptomsPage();
     }
 }
